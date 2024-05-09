339159013b281ad14091d26f9b7a450c6662963f
MirShukhman <miriysha@gmail.com>
2024-05-09 20:09:56 +0300

    class

--- checks.py
+++ checks.py
@@ -7,22 +7,22 @@ from selenium.common.exceptions import TimeoutException
 
 class Checks:
     def __init__(self):
-        self.driver = webdriver.Chrome()
+        pass
         
-    
     def check_pegasus(self):
-        self.driver.maximize_window()
+        driver = webdriver.Chrome()
+        driver.maximize_window()
         try:
-            self.driver.get('http://20.246.179.188:5000/redirect_to_searchforflightbyid')
+            driver.get('http://20.246.179.188:5000/redirect_to_searchforflightbyid')
             
-            input_element = self.driver.find_element("id",'flight_id')
-            submit_button = self.driver.find_element("id",'search_for_flight_by_id_button')
+            input_element = driver.find_element("id",'flight_id')
+            submit_button = driver.find_element("id",'search_for_flight_by_id_button')
 
             input_element.send_keys('13')
 
             submit_button.click()
             # Wait until the "flight" div appears (with a timeout of 10 seconds)
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID, 'flight')))
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'flight')))
             status=f"Action performed successfully"
         
         except TimeoutException:
@@ -33,30 +33,31 @@ class Checks:
             status=f"ERROR :Failed to perform action: {str(e)}"
             
         finally:
-            self.driver.quit()
+            driver.quit()
             return status
 
 
     def check_eventhub(self):
-        self.driver.maximize_window()
+        driver = webdriver.Chrome()
+        driver.maximize_window()
         try:
-            self.driver.get('http://172.214.123.238/')
+            driver.get('http://172.214.123.238/')
 
-            menu_button = self.driver.find_element(By.CSS_SELECTOR,'#open-sidebar button')
+            menu_button = driver.find_element(By.CSS_SELECTOR,'#open-sidebar button')
             menu_button.click()
             
-            sidebar_button = self.driver.find_element(By.CSS_SELECTOR,'.sidebar-buttons-wrapper button')
+            sidebar_button = driver.find_element(By.CSS_SELECTOR,'.sidebar-buttons-wrapper button')
             sidebar_button.click()
             
-            organiser_input = self.driver.find_element("id",'organiser')
+            organiser_input = driver.find_element("id",'organiser')
             organiser_input.send_keys('Max Vol')
 
-            submit_button = self.driver.find_element(By.CSS_SELECTOR, '.search-button button')
+            submit_button = driver.find_element(By.CSS_SELECTOR, '.search-button button')
             submit_button.click()
             submit_button.click()
         
             # Wait until the "open-event" div appears 
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'open-event')))
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'open-event')))
             status=f"Action performed successfully"
         
         except TimeoutException:
@@ -67,31 +68,32 @@ class Checks:
             status=f"ERROR :Failed to perform action: {str(e)}"
             
         finally:
-            self.driver.quit()
+            driver.quit()
             return status
 
 
     def check_resume(self):
-            self.driver.maximize_window()
+            driver = webdriver.Chrome()
+            driver.maximize_window()
             try:
-                self.driver.get('http://20.253.70.24/')
+                driver.get('http://20.253.70.24/')
 
-                name_input = self.driver.find_element(By.CSS_SELECTOR,'#message-form #name')
+                name_input = driver.find_element(By.CSS_SELECTOR,'#message-form #name')
 
-                WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#message-form #name')))
+                WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,'#message-form #name')))
                 name_input.send_keys('twice-a-day-test')
 
-                eamil_input = self.driver.find_element("id",'eamil')
+                eamil_input = driver.find_element("id",'eamil')
                 eamil_input.send_keys('miriysha@gmail.com')
 
-                message_input = self.driver.find_element(By.TAG_NAME, 'textarea')
+                message_input = driver.find_element(By.TAG_NAME, 'textarea')
                 message_input.send_keys('hola :)')
 
-                submit_button = self.driver.find_element(By.CSS_SELECTOR, '#message-form button')
+                submit_button = driver.find_element(By.CSS_SELECTOR, '#message-form button')
                 submit_button.click()
 
                 # Wait until the "sucsess-msg" div appears 
-                WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'sucsess-msg')))
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'sucsess-msg')))
                 status=f"Action performed successfully"
             
             except TimeoutException:
@@ -102,5 +104,5 @@ class Checks:
                 status=f"ERROR :Failed to perform action: {str(e)}"
                 
             finally:
-                self.driver.quit()
+                driver.quit()
                 return status
